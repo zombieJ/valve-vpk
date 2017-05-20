61dc010c588b0c8da9552dad5164453783be43a8
jilin <smith3816@gmail.com>
2017-05-21 00:38:52 +0800

    do reader

--- src/FileReader.ts
+++ src/FileReader.ts
@@ -1,5 +1,5 @@
 const FS = require('fs');
-const BUFFER_MAX_LEN = 1024;
+const BUFFER_MAX_LEN = 4;
 
 class FileReader {
 	path: string;
@@ -10,18 +10,24 @@ class FileReader {
 	bufferLen: number;
 	bufferIndex: number;
 
+	done: boolean = false;
+
 	constructor(path) {
 		this.path = path;
 		this.fileDef = FS.openSync(this.path, 'r');
 	}
 
-	getBuffer(): Buffer {
+	getBuffer(minLen: number = 0): Buffer {
 		if (!this.buffer) {
 			const buffer = new Buffer(BUFFER_MAX_LEN);
 
 			this.bufferLen = FS.readSync(this.fileDef, buffer, 0, BUFFER_MAX_LEN, null);
-			this.buffer = buffer;
-			this.bufferIndex = 0;
+			if (this.bufferLen !== 0) {
+				this.buffer = buffer;
+				this.bufferIndex = 0;
+			} else {
+				this.done = true;
+			}
 		} else if (this.bufferIndex >= this.buffer.length) {
 			this.buffer = null;
 			return this.getBuffer();
@@ -29,10 +35,39 @@ class FileReader {
 		return this.buffer;
 	}
 
+	readUInt8() {
+		if (this.done) return null;
+		const int = this.getBuffer().readUInt8(this.bufferIndex);
+		this.bufferIndex += 1;
+		return int;
+	}
+
+	readUInt16() {
+		if (this.done) return null;
+		const int = this.getBuffer().readUInt16LE(this.bufferIndex);
+		this.bufferIndex += 2;
+		return int;
+	}
+
 	readUInt32() {
+		if (this.done) return null;
 		const int = this.getBuffer().readUInt32LE(this.bufferIndex);
 		this.bufferIndex += 4;
 		return int;
+	}
+
+	readString() {
+		if (this.done) return null;
+
+		let int: number;
+		const arrList: Array<number> = [];
+		while (true) {
+			int = this.readUInt8();
+			if (int === 0) break;
+
+			arrList.push(int);
+		}
+		return String.fromCharCode.apply(null, new Uint8Array(arrList));
 	}
 }
 
--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,81 @@ class VPK {
 	path: string;
 	fileReader: FileReader;
 
+	version: number;
+	treeSize: number;
+
+	fileDataSectionSize: number;
+	archiveMD5SectionSize: number;
+	otherMD5SectionSize: number;
+	signatureSectionSize: number;
+
 	constructor(path) {
 		this.path = PATH.normalize(path);
 		this.fileReader = new FileReader(this.path);
 	}
 
-	load() {
-		return new Promise((resolve, reject) => {
-			// Check file type
-			const signature = this.fileReader.readUInt32();
-			if (signature !== 0x55aa1234) {
-				return reject('Not a VPK file');
+	async load() {
+		await this.loadHeader();
+		await this.loadTree();
+	}
+
+	async loadHeader() {
+		// Check file type
+		const signature = this.fileReader.readUInt32();
+		if (signature !== 0x55aa1234) {
+			throw 'Not a VPK file';
+		}
+
+		// Check version
+		this.version = this.fileReader.readUInt32();
+		if (this.version !== 1 && this.version !== 2) {
+			throw `VPK Version not support: ${this.version}`;
+		}
+
+		// Get tree size
+		this.treeSize = this.fileReader.readUInt32();
+
+		// Version 2 check
+		if (this.version === 2) {
+			this.fileDataSectionSize = this.fileReader.readUInt32();
+			this.archiveMD5SectionSize = this.fileReader.readUInt32();
+			this.otherMD5SectionSize = this.fileReader.readUInt32();
+			this.signatureSectionSize = this.fileReader.readUInt32();
+		}
+	}
+
+	async loadTree() {
+		while (true) {
+			// Extension
+			const extension = this.fileReader.readString();
+			if (!extension) break;
+
+			while (true) {
+				// Path
+				const path = this.fileReader.readString();
+				if (!path) break;
+
+				while (true) {
+					// File Name
+					const fileName = this.fileReader.readString();
+					if (!fileName) break;
+
+					const fullName = `${path}/${fileName}.${extension}`;
+					console.log('=>', fullName);
+
+					console.log('CRC', this.fileReader.readUInt32());
+					// console.log('PreloadBytes', this.fileReader.readUInt16());
+					// console.log('ArchiveIndex', this.fileReader.readUInt16());
+					// console.log('EntryOffset', this.fileReader.readUInt32());
+					// console.log('EntryLength', this.fileReader.readUInt32());
+					// console.log('End', this.fileReader.readUInt16());
+
+					break;
+				}
+				break;
 			}
-			console.log('>>>', signature === 0x55aa1234);
-			resolve();
-		});
+			break;
+		}
 	}
 }
 
